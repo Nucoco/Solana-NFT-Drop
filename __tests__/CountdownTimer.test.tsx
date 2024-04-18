56dd20ecd4f28a37f146e1ebc1bac99a405188a8
Nucoco <yuuta11511997@gmail.com>
2024-04-18 14:54:50 +0000

    テストコード修正

--- __tests__/CountdownTimer.test.tsx
+++ __tests__/CountdownTimer.test.tsx
@@ -5,7 +5,7 @@ import { describe } from 'node:test';
 import CountdownTimer from '../components/CountdownTimer';
 
 beforeEach(() => {
-  // タイマー関数をモックする
+  // 実際のタイマー関数を、時間経過をコントロールできる関数でモックする
   jest.useFakeTimers();
   jest.spyOn(global, 'clearInterval');
 });
@@ -15,54 +15,50 @@ afterEach(() => {
 });
 
 describe('CountdownTimer', () => {
+  // ドロップ日までのカウントダウンが表示されるか
   it('should render the countdown timer', async () => {
-    /** 準備 */
-    /** ドロップ開始時間を、現在の時刻から1分後に設定する */
+    // テスト対象コンポーネントに渡すドロップ開始時間を1秒後に設定
     const dropDate = new Date(Date.now() + 1000 * 60 * 1);
 
     render(<CountdownTimer dropDate={dropDate} />);
 
+    // 1秒進める
     act(() => {
-      /** 1秒タイマーを進める */
       jest.advanceTimersByTime(1000);
     });
 
-    /** 実行 */
+    // テスト
     const textElement = screen.getByText(/Candy Drop Starting In/);
     const textTimerElement = screen.getByText(/⏰ 0d 0h 0m 59s/);
-
-    /** 確認 */
     expect(textElement).toBeInTheDocument();
     expect(textTimerElement).toBeInTheDocument();
   });
 
+  // ドロップ日が過ぎたらタイマーは表示されないか
   it('should clear the interval when the countdown reaches zero', () => {
-    /** 準備 */
-    /** ドロップ開始時間を、現在の時刻から1秒後に設定する */
     const dropDate = new Date(Date.now() + 1000 * 1);
-
     render(<CountdownTimer dropDate={dropDate} />);
 
-    /** 実行 */
+    // 2秒進める
     act(() => {
-      /** 2秒タイマーを進める */
       jest.advanceTimersByTime(2000);
     });
 
-    /** 確認 */
+    // テスト
     const textElement = screen.queryByText(/⏰/);
-
     expect(textElement).toBeNull();
     expect(clearInterval).toHaveBeenCalled();
   });
 
+  // アンマウントされた時にclearIntervalが呼び出されるか
   it('should clear the interval when the component unmounts', async () => {
     const dropDate = new Date(Date.now() + 1000 * 60 * 1);
     const { unmount } = render(<CountdownTimer dropDate={dropDate} />);
 
-    /** コンポーネントをアンマウントする */
+    // コンポーネントをアンマウントする
     unmount();
 
+    // テスト
     expect(clearInterval).toHaveBeenCalled();
   });
 });
